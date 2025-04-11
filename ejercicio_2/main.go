bbecd3102b74d51e90612192b411ce9a5f36be7e
AnaRFG <2304151@ucc.edu.ar>
2025-04-11 12:57:28 -0300

    Se agregó ejercicio de variables por default

---
+++ ejercicio_2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		defaultInt     int
+		defaultUint    uint
+		defautlfloat32 float32
+		defaultbool    bool
+		defaultstirng  string
+	)
+
+	fmt.Println(defaultInt, defaultUint, defautlfloat32, defaultbool, defaultstirng)
+}
