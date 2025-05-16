5a7bb2c2dd4c5639430520b4513ca09d5c571f0a
AnaRFG <2304151@ucc.edu.ar>
2025-05-16 15:56:27 -0300

    act vynil

--- ejercicio_8/database/connect.go
+++ ejercicio_8/database/connect.go
@@ -1,17 +1,30 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var db *gorm.DB
-
 func InitDB() (*gorm.DB, error) {
-	//usuario:password@tcp(ruta:puerto)/baseDeDatos
-	dsn := "root:4818841ro@tcp(localhost:3307)/db_vinilos"
+
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	dsn := fmt.Sprintf("%s:%v@tcp(%s:%v)/%v",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
